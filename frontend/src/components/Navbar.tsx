379e7baeb458f2ff3a86507fad27f74d3dd539f8
TPGCIG <tristan.green.300604@gmail.com>
2025-05-19 15:11:21 +1000

    added some functionality to the frontend

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Navbar() {
+
+    return (
+        <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+
+
+        </nav>
+
+    )
+
+}
